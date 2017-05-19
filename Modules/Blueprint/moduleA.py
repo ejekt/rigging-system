cabb1be47e657c4cbf609e60b7d6e4b3118550ea
ejekt <ejekt@yahoo.com>
2017-05-19 00:11:54 -0700

    Add files via upload
    incrementing index works finally

--- Modules/Blueprint/moduleA.py
+++ Modules/Blueprint/moduleA.py
@@ -11,38 +11,20 @@ TITLE = 'Module A'
 DESCRIPTION = 'Test desc for module A'
 ICON = os.environ['RIGGING_TOOL_ROOT'] + '/Icons/_hand.xpm'
 
-cNamer = n.Name(type='blueprint', mod=CLASS_NAME)
+#cNamer = n.Name(type='blueprint', mod=CLASS_NAME)
 
 class ModuleA:
 	def __init__(self, sName):
 		# module namespace name initialized
-		cNamer.part = sName
-		cNamer.idx = 0
-		cNamer.constructName()
-		self.sName = cNamer.name
+		self.moduleName = CLASS_NAME
+		self.userSpecifiedName = sName
 
-		print 'NAME: ' + self.sName
+		self.moduleNamespace = self.moduleName + '__' + self.userSpecifiedName
+
+		print 'init module namespace: ', self.moduleNamespace
 
 	def install(self):
-		print '\n== MODULE install\t' + CLASS_NAME
+		print '\n== MODULE install class {} using namespace {}'.format(CLASS_NAME, self.moduleNamespace)
 		# check if one already exists and increment the index if it does
 		mc.namespace(setNamespace=':')
-		namespaces = mc.namespaceInfo(listOnlyNamespaces=True)
-		self.partName = 'userGeneratedName'
-		cNamer = n.Name( type='blueprint', mod=CLASS_NAME, part=self.partName)
-		cNamer.constructName()
-		sBaseName = cNamer.name
-
-
-		print 'testing --  ' + sBaseName
-		if mc.namespace(exists=':%s' % sBaseName ):
-			print '\t--------'+ sBaseName	+ ' ---exists'
-			cNamer.idx = cNamer.getIndex() + 1
-			namespaces = mc.namespaceInfo(listOnlyNamespaces=True)
-			print 'findHighestIndex returns: ' + str(utils.findHighestIndex_buzz3d(namespaces, sBaseName))
-			print 'incrementing to:  ' + str(cNamer.idx)
-		
-		cNamer.constructName()
-		print 'cnamer==' + cNamer.name + '\t INSTALLING'
-		self.sName = cNamer.name
-		mc.namespace(add=self.sName)
+		mc.namespace(add=self.moduleNamespace)
