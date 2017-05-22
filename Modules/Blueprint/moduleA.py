f7c1382f14eeb39bc266f08f63e3b30ed5a81c94
ejekt <ejekt@yahoo.com>
2017-05-21 20:56:30 -0700

    chapter 24

--- Modules/Blueprint/moduleA.py
+++ Modules/Blueprint/moduleA.py
@@ -35,7 +35,7 @@ class ModuleA:
 		self.jointsGrp = mc.group(em=True, n=self.moduleNamespace+':joints_grp')
 		self.moduleGrp = mc.group(self.jointsGrp, n=self.moduleNamespace+':module_grp')
 
-		mc.container(n=self.containerName, addNode=[self.moduleGrp], ihb=True)
+		mc.container(n=self.containerName, addNode=[self.moduleGrp], includeHierarchyBelow=True)
 
 		mc.select(cl=True)
 
@@ -52,9 +52,10 @@ class ModuleA:
 			# create the joint and add it to the container
 			jointName_full = mc.joint(n=self.moduleNamespace+':'+jointName, p=jointPos)
 			joints.append(jointName_full)
-			mc.container(self.containerName, edit=True, addNode=jointName_full)
+			
+			utils.addNodeToContainer(self.containerName, jointName_full, ihb=True)
 			mc.container(self.containerName, edit=True, publishAndBind=[jointName_full+'.rotate', jointName+'_R'])
-			mc.container(self.containerName, edit=True, publishAndBind=[jointName_full+'.rotateOrder', jointName+'_rotateOrder'])
+			mc.container(self.containerName, edit=True, publishAndBind=[jointName_full+'.rotateOrder', jointName+'_RO'])
 
 			# orient the parent joint to the newly created joint
 			if index > 0:
@@ -63,4 +64,78 @@ class ModuleA:
 
 			index += 1
 		mc.parent(joints[0], self.jointsGrp, absolute=True)
+
+		# create translation controls for the joints
+		translationControls = []
+		for joint in joints:
+			translationControls.append(self.createTranslationControlAtJoint(joint))
+
+		rootJoint_pCon = mc.pointConstraint(translationControls[0], joints[0], mo=False, n=joints[0]+'_pCon')
+		utils.addNodeToContainer(self.containerName, rootJoint_pCon)
+
+		# setup stretchy joint segement
+		for index in range(len(joints) - 1):
+			self.setupStretchyJointSegment(joints[index], joints[index+1])
+
+		utils.forceSceneUpdate()
+
+		# lock the container
 		mc.lockNode(self.containerName, lock=True, lockUnpublished=True)
+
+
+	def createTranslationControlAtJoint(self, joint):
+		# import the control sphere
+		posControlFile = os.environ['RIGGING_TOOL_ROOT'] + '/ControlObjects/Blueprint/translation_control.ma'
+		mc.file(posControlFile, i=True)
+		# go through and rename all the nodes based on the joint
+		container = mc.rename('translation_control_container', joint+'_translation_control_container')
+		utils.addNodeToContainer(self.containerName, container)	# add it all to this instance's container
+		for node in mc.container(container, q=True, nodeList=True):
+			mc.rename(node, joint+'_'+node, ignoreShape=True)
+		# position the control
+		control = joint+'_translation_control'
+		jointPos = mc.xform(joint, q=True, ws=True, t=True)
+		mc.xform(control, ws=True, absolute=True, t=jointPos)
+		# publish attributes
+		niceName = utils.stripLeadingNamespace(joint)[1]
+		attrName = niceName + '_T'
+
+		mc.container(container, edit=True, publishAndBind=[control+'.t', attrName])
+		mc.container(self.containerName, edit=True, publishAndBind=[container+'.'+attrName, attrName])
+
+		return control
+
+	def getTranslationControl(self, jointName):
+		return jointName + '_translation_control'
+
+	def setupStretchyJointSegment(self, parentJoint, childJoint):
+		parentTranslationControl = self.getTranslationControl(parentJoint)
+		childTranslationControl = self.getTranslationControl(childJoint)
+
+		pvLoc = mc.spaceLocator(n=parentTranslationControl+'pvLoc')[0]
+		pvLocGrp = mc.group(pvLoc, n=pvLoc+'_pConGrp')
+		mc.parent(pvLocGrp, self.moduleGrp, absolute=True)
+		parentConst = mc.parentConstraint(parentTranslationControl, pvLocGrp, mo=False)[0]
+		mc.setAttr(pvLoc+'.v', 0)
+		mc.setAttr(pvLoc+'.ty', -0.5)
+
+
+
+		dIkNodes = utils.basicStretchyIK(parentJoint, 
+					childJoint, 
+					sContainer=self.containerName, 
+					bMinLengthLock=False, 
+					poleVectorObj=pvLoc, 
+					sScaleCorrectionAttr=None)
+
+		ikHandle = dIkNodes['ikHandle']
+		rootLoc = dIkNodes['rootLoc']
+		endLoc = dIkNodes['endLoc']
+
+		child_pCon = mc.pointConstraint(childTranslationControl, endLoc, mo=False, n=endLoc+'_pCon')[0]
+
+		utils.addNodeToContainer(self.containerName, [pvLocGrp,child_pCon], ihb=True)
+
+		for node in [ikHandle, rootLoc, endLoc]:
+			mc.parent(node, self.jointsGrp)
+			mc.setAttr(node+'.v', 0)
